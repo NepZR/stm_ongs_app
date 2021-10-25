5ff2dfb57c1921dc7fcd18d392555118d5530d9f
Octacilio Carvalho de Almeida <octa.oca44@gmail.com>
2021-10-25 08:15:39 -0300

    retirar girl component

--- stm-ongs/src/components/Pages/Cadastro/index.tsx
+++ stm-ongs/src/components/Pages/Cadastro/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Girl from './../../../images/girl-sitting.svg';
 import SignUpForm from "../../SignUpForm";
 import './styles.css'
 import TitleGirl from "../../TiitleGirl";
