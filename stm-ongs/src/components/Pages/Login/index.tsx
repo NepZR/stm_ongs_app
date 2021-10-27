51151ccc12b3fbe1a2cb8bb13b051db0b318a476
Octacilio Carvalho de Almeida <octa.oca44@gmail.com>
2021-10-27 20:57:29 -0300

    Componente Card

--- stm-ongs/src/components/Pages/Login/index.tsx
+++ stm-ongs/src/components/Pages/Login/index.tsx
@@ -4,6 +4,7 @@ import EyeOp from './../../../images/eye.svg'
 import EyeCl from './../../../images/eye-closed.svg'
 import { Link } from 'react-router-dom'
 import Button from "../../Button";
+import Card from "../../Card";
 
 import { useState } from 'react';
 import TitleGirl from "../../TiitleGirl";
@@ -35,12 +36,8 @@ export default function Login() {
     return (
         <>
             <div className="login-container">
-                {/* <section>
-                    <h1 className="title-login-page" >Plataforma <br /> STM ONG's</h1>
-                    <img className="girl-login" src={GirlSitting} alt="" />
-                </section> */}
-                <TitleGirl/>
-                <section className="form-login-container">
+                <TitleGirl />
+                <Card>
                     <h2 className="title-form">Login</h2>
                     <form className="form-login" action="" method="post">
                         <label className="label-login" htmlFor="email">
@@ -62,7 +59,30 @@ export default function Login() {
                         <Link className="link" to="/password-recovery" >Esqueci a senha?</Link>
                         <Link className="link" to="/sign-up" >Não tenho cadastro?</Link>
                     </div>
-                </section>
+                </Card>
+                {/* <section className="form-login-container">
+                    <h2 className="title-form">Login</h2>
+                    <form className="form-login" action="" method="post">
+                        <label className="label-login" htmlFor="email">
+                            E-mail
+                            <input placeholder="Insira o e-mail" className="input-login" type="email" name="email" required />
+                        </label>
+                        <label className="label-login" htmlFor="password">
+                            Password
+                            <div className="password">
+                                <input placeholder="Insira a senha" className="input-login" type="password" name="password" id="password" required />
+                                <button className="icon" type="button" onClick={changeIcon}>
+                                    <img className="icon-svg" src={iconPassword} alt="Icone senha" />
+                                </button>
+                            </div>
+                        </label>
+                        <Button typeBtn="submit">Entrar</Button>
+                    </form>
+                    <div className="links-container">
+                        <Link className="link" to="/password-recovery" >Esqueci a senha?</Link>
+                        <Link className="link" to="/sign-up" >Não tenho cadastro?</Link>
+                    </div>
+                </section> */}
             </div>
         </>
     )
--- stm-ongs/src/components/Pages/PassRecovery/index.tsx
+++ stm-ongs/src/components/Pages/PassRecovery/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Button from "../../Button";
+import Card from "../../Card";
 import Input from "../../Input";
 import TitleGirl from "../../TiitleGirl";
 import './styles.css'
@@ -11,7 +12,20 @@ export default function PassRecovery() {
     return (
         <>
             <div className="container-pass-recovery">
-                <section className="card-pass-recovery">
+                <Card>
+                    <h2 className="card-pass-title">Redefinição de senha</h2>
+                    <p className="card-pass-text">
+                        <strong>Esqueceu a senha ?</strong> Sem problema. Informe o e-mail <strong>cadastrado</strong> no sistema
+                        e receba o link para redefinir sua senha.
+                    </p>
+                    <label htmlFor="">
+                        E-mail
+                        <Input type="email" name="email" placeholder="usermail@gmail.com" />
+                    </label>
+                    <Button typeBtn="submit" children="Enviar" />
+                    <Link style={{ textAlign: 'center', marginTop: '1rem' }} className="link" to="/sign-in">Voltar ao login</Link>
+                </Card>
+                {/* <section className="card-pass-recovery">
                     <h2 className="card-pass-title">Redefinição de senha</h2>
                     <p className="card-pass-text">
                         <strong>Esqueceu a senha ?</strong> Sem problema. Informe o e-mail <strong>cadastrado</strong> no sistema
@@ -23,12 +37,8 @@ export default function PassRecovery() {
                     </label>
                     <Button typeBtn="submit" children="Enviar" />
                     <Link style={{textAlign: 'center',marginTop:'1rem'}} className="link" to="/sign-in">Realizar login</Link>
-                </section>
-                {/* <section className="img-title-container">
-                    <h1 className="title">Plataforma <br /> STM ONG's</h1>
-                    <img className="girl" src={Girl} alt="Mulher sentada em uma poltrona com um notebook" />
                 </section> */}
-                <TitleGirl/>
+                <TitleGirl />
             </div>
 
         </>
