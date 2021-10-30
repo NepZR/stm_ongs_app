fdd5cba808dcfaaf187fc13f0bec685822428e90
Octacilio Carvalho de Almeida <octa.oca44@gmail.com>
2021-10-30 17:52:52 -0300

    Button radio para seleção de tipo de form

--- stm-ongs/src/components/SignUpForm/index.tsx
+++ stm-ongs/src/components/SignUpForm/index.tsx
@@ -4,8 +4,9 @@ import Input from "../Input";
 import './styles.css';
 import Button from "../Button";
 
-// import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Card from "../Card";
+// import CheckBox from "../Checkbox";
 
 
 const form = {
@@ -15,26 +16,47 @@ const form = {
 
 export default function SignUpForm() {
 
-    // const [type, setType] = useState(form.FISIC);
+    const [type, setType] = useState(form.FISIC);
     //Apenas para fins de teste de deploy
-    const type = 'cpf'
-    // function setFormType() {
-    //     type === form.FISIC ? setType(form.JURIDIC) : setType(form.FISIC)
-    // }
+    // const type = 'cpf'
+    function setFormType() {
+        if(type === form.FISIC) {
+            setType(form.JURIDIC)
+        } else { 
+            setType(form.FISIC)
+        }
+    }
+
 
     return (
         <>
             <Card>
                 <h1 className="title-singup">Sign-up</h1>
+                <section className="options-container">
+                    <label htmlFor="type-user-1">
+                        <input onClick={setFormType} className="input-radio" type="radio" name="type-user" id="cpf" defaultChecked/>
+                        Doador
+                    </label>
+                    <label htmlFor="type-user-2">
+                        <input style={{backgroundColor: "black"}} onClick={setFormType} className="input-radio" type="radio" name="type-user" id="cnpj" />
+                        ONG
+                    </label>
+                </section>
                 <form className="form-singup" action="" method="post">
                     <Input label="Nome" type="text" placeholder="Nome" name="name" />
-                    <Input label={type} type="text" placeholder={type} name={type} />
+                    <Input label={type.toUpperCase()} type="text" placeholder={type.toUpperCase()} name={type} />
                     <Input label="E-mail" type="email" placeholder="E-mail" name="email" />
                     <InputPassword name="senha" placeholder="Senha" id="senha" />
                     <InputPassword name="conf_senha" placeholder="Confirmar" id="conf_senha" />
                     <Button typeBtn="submit" children="Cadastrar" />
                 </form>
-                <Link className="link" style={{ textAlign: "center", marginTop: '1rem' }} to="/sign-in"> Já possuo cadastro?</Link>
+                <Link
+                    className="link"
+                    style={{ textAlign: "center", marginTop: '1rem' }}
+                    to="/sign-in"
+                >
+                    Já possuo cadastro?
+                </Link>
             </Card>
 
         </>
