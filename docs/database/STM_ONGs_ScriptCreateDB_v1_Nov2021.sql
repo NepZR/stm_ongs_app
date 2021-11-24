c7f0f0fa08727d89b1bba75369fe304681485782
NepZR <lucas.darlindo@gmail.com>
2021-11-24 20:52:14 -0300

    Documentação e Utils: BD

---
+++ docs/database/STM_ONGs_ScriptCreateDB_v1_Nov2021.sql
@@ -0,0 +1,12 @@
+-- Database: stm_ongs
+
+-- DROP DATABASE IF EXISTS stm_ongs;
+
+CREATE DATABASE stm_ongs
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Portuguese_Brazil.1252'
+    LC_CTYPE = 'Portuguese_Brazil.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
