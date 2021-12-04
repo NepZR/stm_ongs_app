a7702d8e0f189226dd3912eb0c20fe7d3ce607c6
Octacilio C. de Almeida <octa.oca44@gmail.com>
2021-12-04 14:35:31 -0300

    Useform implementado

--- stm-ongs/src/pages/NovaCampanha/index.tsx
+++ stm-ongs/src/pages/NovaCampanha/index.tsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../../components/Button";
 import Input from "../../components/Input";
 import Menu from "../../components/Menu";
 import NavBar from "../../components/NavBar";
 import './styles.css'
+import { useForm } from 'react-hook-form';
+
+
+interface ISubCampaing {
+    title_campaing: string;
+    description: string;
+    limit_date: string;
+    value: string;
+    cover: string;
+}
+
+const form = { ONLINE: "online", PRES: "pres" }
 
 export default function NovaCampanha() {
+
+    const { handleSubmit, register, setValue } = useForm();
+    const [type, setType] = useState(form.PRES);
+
+    const submitCampaing = (data: ISubCampaing) => {
+        console.log(data)
+    }
+
+    function setOn() {
+        setType(form.ONLINE);
+    }
+
+    function setPres() {
+        setType(form.PRES);
+    }
 
     return (
         <>
@@ -13,34 +40,96 @@ export default function NovaCampanha() {
             <Menu />
             <div className="new-campaing-container">
 
-                <section className="card-campaing-form">
-                    <h1 className="title-form"> Nova <br />Campanha</h1>
+                <section
+                    className="card-campaing-form"
+                >
+                    <h1
+                        className="title-form"
+                    > Nova <br />Campanha</h1>
 
                     <section className="type-campaing-container" >
                         <label htmlFor="type-campaing-1">
-                            <input className="radio-button" type="radio" name="type-campaing" id="type-campaing-1" />
+                            <input
+                                className="radio-button"
+                                type="radio"
+                                name="type-campaing"
+                                id="type-campaing-1"
+                                defaultChecked
+                                onClick={setPres}
+                            />
                             Presencial
                         </label>
 
                         <label htmlFor="type-campaing-2">
-                            <input className="radio-button" type="radio" name="type-campaing" id="type-campaing-2" />
+                            <input
+                                className="radio-button"
+                                type="radio"
+                                name="type-campaing"
+                                id="type-campaing-2"
+                                onClick={setOn}
+                            />
                             Online
                         </label>
                     </section>
 
-                    <form className="form-campaing" action="">
-                        <Input id="title-campaing" label="Titulo da Campanha" placeholder="Compra de ração" type="text" name="title-campaing" />
-                        <Input id="description" label="Descrição" placeholder="Compra de ração" type="text" name="description" />
-                        <div className="date-value-container" style={{ flexDirection: 'row', display: 'flex', justifyContent: 'space-between' }}>
-                            <Input id="limit-date" label="Data limite" placeholder="" type="date" name="limit-date" />
-                            <Input id="value" label="Valor" placeholder="R$ 0,00" type="number" name="value" />
+                    <form
+                        className="form-campaing"
+                        onSubmit={handleSubmit(submitCampaing)}
+                    >
+                        <input
+                            {...register("type_campaing")}
+                            type='hidden'
+                            name="type_campaing"
+                            value={type}
+                            {...type === form.ONLINE ? setValue("type_campaing", form.ONLINE) : setValue("type_campaing", form.PRES)}
+                        />
+
+                        <Input
+                            rest={{ ...register("title_campaing") }}
+                            id="title-campaing"
+                            label="Titulo da Campanha"
+                            placeholder="Compra de ração"
+                            type="text"
+                            name="title_campaing"
+                        />
+                        <Input
+                            rest={{ ...register("description") }}
+                            id="description"
+                            label="Descrição"
+                            placeholder="Compra de ração"
+                            type="text"
+                            name="description"
+                        />
+                        <div
+                            className="date-value-container"
+                            style={{ flexDirection: 'row', display: 'flex', justifyContent: 'space-between' }}
+                        >
+                            <Input
+                                rest={{ ...register("limit_date") }}
+                                id="limit-date"
+                                label="Data limite"
+                                placeholder=""
+                                type="date"
+                                name="limit_date"
+                            />
+
+                            {type === form.ONLINE && <Input rest={{ ...register("value") }} id="value" label="Valor" placeholder="R$ 0,00" type="number" name="value" />}
+                            {type === form.PRES && <Input rest={{ ...register("value") }} id="value" label="Local" placeholder="Ex: Av Rui Barbosa" type="number" name="value" />}
                         </div>
 
-                        <div className="input-file-container">
+                        <div
+                            className="input-file-container"
+                        >
                             <label htmlFor="cover">Carregar Arquivo</label>
-                            <input className="input-file" id="cover" placeholder="Compra de ração" type="file" name="cover" />
+                            <input
+                                {...register("cover")}
+                                className="input-file"
+                                id="cover"
+                                placeholder="Compra de ração"
+                                type="file"
+                                name="cover"
+                            />
                         </div>
-
 
                         <Button>Cadastrar Campanha</Button>
 
@@ -49,5 +138,4 @@ export default function NovaCampanha() {
             </div>
         </>
     )
-
 }
