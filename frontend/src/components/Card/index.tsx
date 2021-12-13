46f2343abeddcaa25e79bd79a4797bcaac200294
Octacilio C. Ameida <octa.oca44@gmail.com>
2021-12-13 11:20:38 -0300

    Pasta frontend antiga deletada

--- frontend/src/components/Card/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import './styles.css'
-export default function Card(props: any){
-    return (
-        <div className="card">
-            {props.children}
-        </div>
-    )
-}
